f6081190a2d3a520a62b2b55381560c2c1e14123
Karla Castillo <karlacastbg03@gmail.com>
2025-04-30 15:49:31 -0700

    started homescreen class, created switch statement for homescreen. added transactions.csv

--- src/AccountingLedger.java
+++ src/AccountingLedger.java
@@ -2,9 +2,37 @@ import java.util.Scanner;
 
 public class AccountingLedger {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome!, Please type your selection below!");
+    private static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        boolean running = true;
+        while (running) {
+            displayHomeScreen();
+            String choice = scanner.nextLine().toUpperCase();
+            switch (choice) {
+                case "D":
+                    System.out.println("Add Deposit selected");
+                    break;
+                case "P":
+                    System.out.println("Make Payment selected");
+                    break;
+                case "L":
+                    System.out.println("Ledger selected");
+                    break;
+                case "X":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+        scanner.close();
+    }
+
+    private static void displayHomeScreen() {
+        // Code to display the home screen menu
+    }
+
+    private static void saveTransactions() {
     }
 }
