5a09b34a24c3fffce6812b55eb347a8bfbb7bfb0
Karla Castillo <karlacastbg03@gmail.com>
2025-04-30 15:48:17 -0700

    Initial commit

---
+++ src/AccountingLedger.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AccountingLedger {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome!, Please type your selection below!");
+
+    }
+}
